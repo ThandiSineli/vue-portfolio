1c5e871cb4fd0c23a215f448a214f176f85d9c85
Thandeka Sineli <Thandekasineli@gmail.com>
2024-01-12 14:45:33 +0200

    edit

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,30 +1,34 @@
 <template>
   <h1>My Projects</h1>
-    <div class="projects">
-        <div v-for="project in projects" :key="project.id" class="card" style="width: 27rem;">
-            <img :src=" project.imgUrl" class="card-img-top" :alt="projects.name">
-            <div class="card-body">
-              <h5 class="card-title">{{project.name}}</h5>
-              <a :href="project.github" target="_blank"  class="btn btn-primary">Source code</a>
-              <a :href="project.netlify" target="_blank" class="btn btn-primary">Go Live</a>
-            </div>
-          </div>
+  <div class="projects">
+    <div v-for="project in projects" :key="project.id" class="card" style="width: 27rem;">
+      <img :src="project.imgUrl" class="card-img-top" :alt="project.name">
+      <div class="card-body">
+        <h5 class="card-title">{{project.name}}</h5>
+        <a :href="project.github" target="_blank" class="btn btn-github">
+          <i class="bi bi-github"></i> GitHub
+        </a>
+        <a :href="project.netlify" target="_blank" class="btn btn-github">
+          Go Live
+        </a>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
   computed: {
     projects() {
-      return this.$store.state.projects
+      return this.$store.state.projects;
     },
   },
   mounted() {
     this.$store.dispatch('fetchprojects');
   },
 };
-
 </script>
+
 <style scoped>
 .projects {
   display: flex;
@@ -56,21 +60,32 @@ export default {
   margin-bottom: 0.5rem;
 }
 
-.btn-primary {
-  background-color: rgb(178, 173, 173);
-  color: #fff;
-  border: none;
-  padding: 0.5rem 1rem;
-  border-radius: 5px;
+.btn {
   text-decoration: none;
   margin: 7px;
   border-radius: 0.25rem;
   transition: background-color 0.3s ease;
-  box-shadow: 4px 8px 0px rgb(110, 109, 109) ; 
+  box-shadow: 4px 8px 0px rgb(110, 109, 109);
 }
 
-.btn-primary:hover {
-  background-color: #F3DEBA;
+.btn-github {
+  background-color: #24292e;
+  color: #fff;
+  border: none;
+  padding: 0.5rem 1rem;
+  border-radius: 5px;
+}
+
+.github-icon {
+  width: 20px; /* Set the width and height of the image as needed */
+  height: 20px;
+}
+
+.btn:hover {
+  box-shadow: 4px 8px 0px #666;
+}
+
+.btn-github:hover {
+  background-color: #6e5494;
 }
 </style>
-
